299d20e72455af2a12127c954c0ac028aae61865
Valmor F. de Almeida <dealmeidavf@gmail.com>
2020-05-16 18:56:31 -0400

    NB updates

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('requirements.txt', 'r') as fh:
 
 setuptools.setup(
     name='covid-surge',
-    version='0.0.19',
+    version='0.0.20',
     author='Valmor F. de Almeida',
     author_email='valmor_dealmeida@uml.edu',
     description='Covid-Surge is a utility for computing and comparing surge periods\
